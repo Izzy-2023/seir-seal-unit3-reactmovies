e92a4f209829fc21740a549367f8e74d0a4cb08a
izzy-2023 <izzyzinxhirija@gmail.com>
2024-01-28 10:01:10 -0500

    created react movies api

---
+++ src/components/MovieDisplay.js
@@ -0,0 +1,24 @@
+// We Must Import the React Library
+import React from "react";
+
+// Define a function that is our component, always make sure to declare the props parameter so you can use props in your component
+// You can also destructure your props directly from the parameter list
+const MovieDisplay = ({ movie }) => {
+    // Check if the movie is null before accessing its properties
+    if (!movie) {
+      return <div>No movie data available</div>;
+    }
+  //The component must return some JSX
+  return (
+    <>
+      <h1>{movie.Title}</h1>
+      <h2>{movie.Genre}</h2>
+      <img src={movie.Poster} alt={movie.Title} />
+      <h2>{movie.Year}</h2>
+    </>
+  );
+};
+
+// We must export the component to use it in other files
+export default MovieDisplay;
+
